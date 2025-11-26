1cdc760b31576606299c4477915ac1f5da6ab7d0
Bryan Albert <bryan@Bryans-Mac-mini.local>
2025-11-26 14:10:32 -0800

    Edit type any to unknown

--- src/components/ConnectWalletPrompt.tsx
+++ src/components/ConnectWalletPrompt.tsx
@@ -16,8 +16,8 @@ const CONTRACT_ABI = contractJson.abi;
 interface EthereumProvider {
   isMetaMask?: boolean;
   request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
-  on?: (event: string, handler: (...args: any[]) => void) => void;
-  removeListener?: (event: string, handler: (...args: any[]) => void) => void;
+  on?: (event: string, handler: (...args: unknown[]) => void) => void;
+  removeListener?: (event: string, handler: (...args: unknown[]) => void) => void;
 }
 
 declare global {
