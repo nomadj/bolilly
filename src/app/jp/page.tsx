062030cb113b4853aa96441531ad71cca10a1f3b
nomadj <bryan2j@gmail.com>
2026-08-18 15:29:26 -0700

    Add video thumbnails

--- src/app/jp/page.tsx
+++ src/app/jp/page.tsx
@@ -5,11 +5,12 @@ import ReactPlayer from "react-player";
 
 export default function JpoPage() {
   const videos = [
-    // {
-    //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmVgBa7HTB4saDDGeXDG8NhW9jLZMTKu3aVoNbksWQAyN3",
-    //   title: "JP at the FB",
-    //   desc: "Better Man by Robbie Williams. Ferry Building SF, 08/15/2026",
-    // },
+    {
+      url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmVgBa7HTB4saDDGeXDG8NhW9jLZMTKu3aVoNbksWQAyN3",
+      title: "Better Man",
+      desc: "A Sunday evening at the Ferry Building, San Francisco, CA 08/15/2026",
+      img: "/jp_still_photo.jpg"
+    },
     // {
     //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmNcUuXRbABRYfT54tFkxaZTCNHToevsbtV2NagsDhDSgV",
     //   title: "JP at the FB",
@@ -20,11 +21,12 @@ export default function JpoPage() {
     //   title: "JP at the FB",
     //   desc: "By the Way by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
     // },
-    // {
-    //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/Qmak8ZDaUXPyX2kCLP4R3AuQRtsgDgBvW4SrG2o8KxtBEf",
-    //   title: "JP at the FB",
-    //   desc: "Under the Bridge by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
-    // },
+    {
+      url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/Qmak8ZDaUXPyX2kCLP4R3AuQRtsgDgBvW4SrG2o8KxtBEf",
+      title: "Under the Bridge",
+      desc: "A Sunday evening at the Ferry Building, San Francisco, CA 08/15/2026",
+      img: "/jp_image3.png"
+    },
     // {
     //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmeyDoxfDeg465fM83x2qhvRznHdP7vHyGYjdtotgzbVJC",
     //   title: "JP at the FB",
@@ -35,7 +37,7 @@ export default function JpoPage() {
 
   return (
     <Box p={8}>
-      <Heading ml={20} mb={6}>JP</Heading>
+      <Heading size="3xl" ml={20} mb={6}>JP</Heading>
       <SimpleGrid
 	columns={{ base: 1, sm: 1, md: 3 }}
 	gap={6}
@@ -56,13 +58,13 @@ export default function JpoPage() {
 	    }}
 	  >
 	    {/*Responsive wrapper*/}
-	    <Box position="relative" width="100%" paddingTop="56.25%" mb={3} borderRadius="lg" overflow="hidden">
+	    <Box width="100%" aspectRatio="16/9" mb={3} borderRadius="lg" overflow="hidden">
 	      <ReactPlayer
 		src={vid.url}
 		controls
 		width="100%"
 		height="100%"
-	        style={{ position: "absolute", top: 0, left: 0 }}
+	        light={vid.img}
 	      />
 	    </Box>
 
