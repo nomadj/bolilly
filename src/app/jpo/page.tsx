7d00fbf8baa84f706f855fe1397e6c516e2e97b5
nomadj <bryan2j@gmail.com>
2026-08-18 13:43:47 -0700

    Remove jpo vids

--- src/app/jpo/page.tsx
+++ src/app/jpo/page.tsx
@@ -5,31 +5,32 @@ import ReactPlayer from "react-player";
 
 export default function JpoPage() {
   const videos = [
-    {
-      url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmVgBa7HTB4saDDGeXDG8NhW9jLZMTKu3aVoNbksWQAyN3",
-      title: "JP at the FB",
-      desc: "Better Man by Robbie Williams. Ferry Building SF, 08/15/2026",
-    },
-    {
-      url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmNcUuXRbABRYfT54tFkxaZTCNHToevsbtV2NagsDhDSgV",
-      title: "JP at the FB",
-      desc: "Snow by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
-    },
-    {
-      url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmZLoRtxiNet78h8mNnQJAaMaufATHXJpD785T73HAV93W",
-      title: "JP at the FB",
-      desc: "By the Way by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
-    },
-    {
-      url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/Qmak8ZDaUXPyX2kCLP4R3AuQRtsgDgBvW4SrG2o8KxtBEf",
-      title: "JP at the FB",
-      desc: "Under the Bridge by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
-    },
-    {
-      url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmeyDoxfDeg465fM83x2qhvRznHdP7vHyGYjdtotgzbVJC",
-      title: "JP at the FB",
-      desc: "Soul to Squeeze by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
-    },    
+    // {
+    //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmVgBa7HTB4saDDGeXDG8NhW9jLZMTKu3aVoNbksWQAyN3",
+    //   title: "JP at the FB",
+    //   desc: "Better Man by Robbie Williams. Ferry Building SF, 08/15/2026",
+    // },
+    // {
+    //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmNcUuXRbABRYfT54tFkxaZTCNHToevsbtV2NagsDhDSgV",
+    //   title: "JP at the FB",
+    //   desc: "Snow by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
+    // },
+    // {
+    //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmZLoRtxiNet78h8mNnQJAaMaufATHXJpD785T73HAV93W",
+    //   title: "JP at the FB",
+    //   desc: "By the Way by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
+    // },
+    // {
+    //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/Qmak8ZDaUXPyX2kCLP4R3AuQRtsgDgBvW4SrG2o8KxtBEf",
+    //   title: "JP at the FB",
+    //   desc: "Under the Bridge by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
+    // },
+    // {
+    //   url: "https://violet-terrible-goose-933.mypinata.cloud/ipfs/QmeyDoxfDeg465fM83x2qhvRznHdP7vHyGYjdtotgzbVJC",
+    //   title: "JP at the FB",
+    //   desc: "Soul to Squeeze by Red Hot Chili Peppers. Ferry Building SF, 08/15/2026",
+    // },  
+    
   ];
 
   return (
